ae28f1aa66a75a13e81c9bfa7fc517c539ffdea8
BadiRodriguez <166084491+Badi-Rodriguez@users.noreply.github.com>
2024-07-14 15:46:15 -0500

    a
    AHHHHHHHHHHHH

---
+++ src/tools/MovieContainer.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Badi on 7/14/2024.
+//
+
+#include <iostream>
+#include <vector>
+
+class Container{
+private:
+    std::string vessel;
+    std::string target_directory;
+    std::vector<std::string> container_rolls
+public:
+
+};
