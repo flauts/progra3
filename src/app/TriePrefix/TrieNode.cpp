4dcd02dfa088cade606acad38d187049d7a78665
joruughen <166084525+joruughen@users.noreply.github.com>
2024-07-14 03:09:52 -0500

    Funciona2

--- src/app/TriePrefix/TrieNode.cpp
+++ src/app/TriePrefix/TrieNode.cpp
@@ -44,35 +44,44 @@ void TrieNode::insert_movies_key(const std::vector<std::string>& key, Movie* mov
         }
     }
 
+
 }
 
 
 unordered_set<Movie*>TrieNode::search_movies_by_key(const std::string& key) { // Modificación aquí
     TrieNode* currentNode = this;
+    vector<string> words = Utils::splitString(key);
+    unordered_set<Movie*> movies;
+    for (auto e: words) {
+        currentNode=this;
+        for (auto c: e) {
+            int index;
+            if (!isalnum(c)) { continue; }
+            if (isdigit(c)) {
+                index = c - '0' + 26;
+            } else {
+                c = tolower(c);
+                index = c - 'a';
+            }
 
-    for (auto c : key) {
-        int index;
-        if (isdigit(c)) {
-            index = c - '0' + 26;
-        } else {
-            index = c - 'a';
+            if (currentNode->childNode[index] == nullptr) {
+                return {}; // Devuelve un vector vacío si la clave no existe
+            }
+
+            currentNode = currentNode->childNode[index];
         }
 
-        if (currentNode->childNode[index] == nullptr) {
-            return {}; // Devuelve un vector vacío si la clave no existe
+        if (currentNode->childNode[36] != nullptr) {
+            TrieNodeVector *movieNode = dynamic_cast<TrieNodeVector *>(currentNode->childNode[36]);
+            if (movieNode) {
+                for (auto mov : movieNode->vectorPelis) {
+                    movies.insert(mov);
+                }
+            }
         }
-
-        currentNode = currentNode->childNode[index];
     }
 
-    if (currentNode->childNode[36] != nullptr) {
-        TrieNodeVector* movieNode = dynamic_cast<TrieNodeVector*>(currentNode->childNode[36]);
-        if (movieNode) {
-            return movieNode->vectorPelis;
-        }
-    }
-
-    return {}; // Devuelve un vector vacío si no hay películas asociadas con la clave
+    return movies; // Devuelve un vector vacío si no hay películas asociadas con la clave
 }
 
 void TrieNode::insert_movies_synopsis(const std::string& key, Movie* mov) {
