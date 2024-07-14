1d35565621e2f3f8bbfad6a3cf9d3900c803ddc9
BadiRodriguez <166084491+Badi-Rodriguez@users.noreply.github.com>
2024-07-14 15:54:06 -0500

    Hate
    Hate

--- src/app/TriePrefix/TrieNode.cpp
+++ src/app/TriePrefix/TrieNode.cpp
@@ -1,8 +1,7 @@
 #include "TrieNode.h"
 #include "../../tools/Utils.h"
 
-
-TrieNode::TrieNode() {
+TrieNode::TrieNode() : pool(4) { // Initialize thread pool with 4 threads
     wordEnd = false;
     movieNode = nullptr;
     for (int i = 0; i < 37; i++) {
@@ -10,44 +9,46 @@ TrieNode::TrieNode() {
     }
 }
 
-
-//cada tag es todo junto sin espacios
-//paralelismo con el insert
-
-
-void TrieNode::insert_movies_key(const std::vector<std::string>& key, Movie* mov) { // Modificación aquí
-    TrieNode* currentNode = this;
-    for(auto e: key) {
-        for (auto c: e) {
-            int index;
-            if(!isalnum(c)){continue;}
-            if (isdigit(c)) {
-                index = c - '0' + 26;
-            } else {
-                index = c - 'a';
+void TrieNode::insert_movies_key(const std::vector<std::string>& key, Movie* mov) {
+    std::vector<std::future<void>> futures;
+    for (const auto& e : key) {
+        futures.emplace_back(pool.enqueue([&, e] {
+            TrieNode* currentNode = this;
+            for (auto c : e) {
+                int index;
+                if (!isalnum(c)) { continue; }
+                if (isdigit(c)) {
+                    index = c - '0' + 26;
+                } else {
+                    index = c - 'a';
+                }
+                std::lock_guard<std::mutex> lock(nodeMutex);
+                if (currentNode->childNode[index] == nullptr) {
+                    TrieNode *newNode = new TrieNode();
+                    currentNode->childNode[index] = newNode;
+                }
+                currentNode = currentNode->childNode[index];
             }
-            if (currentNode->childNode[index] == nullptr) {
-                TrieNode *newNode = new TrieNode();
-                currentNode->childNode[index] = newNode;
+
+            if (currentNode->childNode[36] == nullptr) {
+                TrieNodeVector *newNode = new TrieNodeVector();
+                currentNode->childNode[36] = newNode;
             }
-            currentNode = currentNode->childNode[index];
-        }
 
-        if (currentNode->childNode[36] == nullptr) {
-            TrieNodeVector *newNode = new TrieNodeVector();
-            currentNode->childNode[36] = newNode;
-        }
-
-        TrieNodeVector* movieNode = dynamic_cast<TrieNodeVector*>(currentNode->childNode[36]);
-        if (movieNode) {
-            movieNode->vectorPelis.insert(mov);
-        }
+            TrieNodeVector* movieNode = dynamic_cast<TrieNodeVector*>(currentNode->childNode[36]);
+            if (movieNode) {
+                std::lock_guard<std::mutex> lock(nodeMutex);
+                movieNode->vectorPelis.insert(mov);
+            }
+        }));
     }
 
+    for (auto& future : futures) {
+        future.get();
+    }
 }
 
-
-unordered_set<Movie*>TrieNode::search_movies_by_key(const std::string& key) { // Modificación aquí
+unordered_set<Movie*> TrieNode::search_movies_by_key(const std::string& key) {
     TrieNode* currentNode = this;
 
     for (auto c : key) {
@@ -76,11 +77,13 @@ unordered_set<Movie*>TrieNode::search_movies_by_key(const std::string& key) { //
 }
 
 void TrieNode::insert_movies_synopsis(const std::string& key, Movie* mov) {
-    TrieNode* currentNode = this;
-        vector<string> words = Utils::splitString(key);
-            for (auto e: words) {
-                currentNode = this;
-                for(auto c: e) {
+    std::vector<std::string> words = Utils::splitString(key);
+    std::vector<std::future<void>> futures;
+
+    for (const auto& e : words) {
+        futures.emplace_back(pool.enqueue([&, e] {
+            TrieNode* currentNode = this;
+            for (auto c : e) {
                 int index;
                 if (!isalnum(c)) { continue; }
                 if (isdigit(c)) {
@@ -89,19 +92,104 @@ void TrieNode::insert_movies_synopsis(const std::string& key, Movie* mov) {
                     c = tolower(c);
                     index = c - 'a';
                 }
+                std::lock_guard<std::mutex> lock(nodeMutex);
                 if (currentNode->childNode[index] == nullptr) {
                     TrieNode *newNode = new TrieNode();
                     currentNode->childNode[index] = newNode;
                 }
                 currentNode = currentNode->childNode[index];
             }
+
             if (currentNode->childNode[36] == nullptr) {
                 TrieNodeVector *newNode = new TrieNodeVector();
                 currentNode->childNode[36] = newNode;
             }
+
             TrieNodeVector* movieNode = dynamic_cast<TrieNodeVector*>(currentNode->childNode[36]);
             if (movieNode) {
+                std::lock_guard<std::mutex> lock(nodeMutex);
                 movieNode->vectorPelis.insert(mov);
+            }
+        }));
+    }
+
+    for (auto& future : futures) {
+        future.get();
+    }
+}
+
+TrieNodeVector::TrieNodeVector(const std::unordered_set<Movie *> &vectorPelis) : vectorPelis(vectorPelis) {}
+
+void TrieNode::serialize(std::ofstream& out) const {
+    // Write wordEnd
+    out.write(reinterpret_cast<const char*>(&wordEnd), sizeof(bool));
+
+    // Write if movieNode exists
+    bool hasMovieNode = (movieNode != nullptr);
+    out.write(reinterpret_cast<const char*>(&hasMovieNode), sizeof(bool));
+
+    // If movieNode exists, serialize TrieNodeVector
+    if (hasMovieNode) {
+        TrieNodeVector* movieNodeVector = dynamic_cast<TrieNodeVector*>(movieNode);
+        if (movieNodeVector) {
+            size_t movieCount = movieNodeVector->vectorPelis.size();
+            out.write(reinterpret_cast<const char*>(&movieCount), sizeof(size_t));
+
+            // Here you would serialize each Movie* in vectorPelis
+            // For this example, we'll just write the pointer addresses
+            for (const Movie* movie : movieNodeVector->vectorPelis) {
+                out.write(reinterpret_cast<const char*>(&movie), sizeof(Movie*));
             }
         }
     }
+
+    // Write child nodes
+    for (int i = 0; i < 37; ++i) {
+        bool hasChild = (childNode[i] != nullptr);
+        out.write(reinterpret_cast<const char*>(&hasChild), sizeof(bool));
+        if (hasChild) {
+            childNode[i]->serialize(out);
+        }
+    }
+}
+
+TrieNode* TrieNode::deserialize(std::ifstream& in) {
+    TrieNode* node = new TrieNode();
+
+    // Read wordEnd
+    in.read(reinterpret_cast<char*>(&node->wordEnd), sizeof(bool));
+
+    // Read if movieNode exists
+    bool hasMovieNode;
+    in.read(reinterpret_cast<char*>(&hasMovieNode), sizeof(bool));
+
+    // If movieNode exists, deserialize TrieNodeVector
+    if (hasMovieNode) {
+        TrieNodeVector* movieNodeVector = new TrieNodeVector();
+        size_t movieCount;
+        in.read(reinterpret_cast<char*>(&movieCount), sizeof(size_t));
+
+        // Here you would deserialize each Movie* in vectorPelis
+        // For this example, we're just reading the pointer addresses
+        for (size_t i = 0; i < movieCount; ++i) {
+            Movie* movie;
+            in.read(reinterpret_cast<char*>(&movie), sizeof(Movie*));
+            movieNodeVector->vectorPelis.insert(movie);
+        }
+
+        node->movieNode = movieNodeVector;
+    }
+
+    // Read child nodes
+    for (int i = 0; 37; ++i) {
+        bool hasChild;
+        in.read(reinterpret_cast<char*>(&hasChild), sizeof(bool));
+        if (hasChild) {
+            node->childNode[i] = deserialize(in);
+        } else {
+            node->childNode[i] = nullptr;
+        }
+    }
+
+    return node;
+}
--- src/app/TriePrefix/TrieNode.h
+++ src/app/TriePrefix/TrieNode.h
@@ -3,25 +3,42 @@
 
 #include <bits/stdc++.h>
 #include "../Movie/Movie.h"
-using namespace std;
+#include <iostream>
+#include <fstream>
+#include <queue>
+#include <string>
+#include <mutex>
+#include <thread>
+#include "ThreadPool.h"
+
+class TrieNodeVector;
 
 struct TrieNode {
-    TrieNode* childNode[37]; // 26 letters + 10 digits + 1 special node
+    TrieNode* childNode[37];
     bool wordEnd;
-    TrieNode* movieNode; // Puntero a TrieNodeVector
+    TrieNode* movieNode;
+    std::mutex nodeMutex;
+    ThreadPool pool;
 
     TrieNode();
-    virtual ~TrieNode() = default; // Destructor virtual
+    virtual ~TrieNode() = default;
 
-    void insert_movies_key(const std::vector<std::string>& key, Movie* mov); // Modificación aquí
-    unordered_set<Movie*> search_movies_by_key(const std::string& key); // También modificamos aquí
-    void insert_movies_synopsis(const std::string &key, Movie *mov);
+    std::unordered_set<Movie*> search_movies_by_key(const std::string& key);
+    void insert_movies_key(const std::vector<std::string>& key, Movie* mov);
+    void insert_movies_synopsis(const std::string& key, Movie* mov);
+
+    // Serialization method
+    void serialize(std::ofstream& out) const;
+
+    // Deserialization method
+    static TrieNode* deserialize(std::ifstream& in);
 };
 
 struct TrieNodeVector : public TrieNode {
-    unordered_set<Movie*> vectorPelis = unordered_set<Movie*>();
+    std::unordered_set<Movie*> vectorPelis = std::unordered_set<Movie*>();
     TrieNodeVector() = default;
-};
 
+    explicit TrieNodeVector(const std::unordered_set<Movie *> &vectorPelis);
+};
 
 #endif // PROGRA3_TRIENODE_H
