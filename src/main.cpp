11a6a679d48f3079810626d326d25359d514d698
joruughen <166084525+joruughen@users.noreply.github.com>
2024-07-14 17:23:14 -0500

    si

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,18 @@
 #include "app/Movie/Movie.h"
 #include "app/SearchEngine/SearchEngineBuilder.h"
 #include "tools/Utils.h"
+#include <thread>
+#include <vector>
 #include <omp.h>
+
 
 int main() {
 
     auto* Trie = new TrieNode();
+    auto* TrieTags = new TrieNode();
     std::unordered_set<Movie*> movies;
 
-    std::ifstream database("C:\\Users\\flauta\\progra3\\proyecto\\progra3\\datos.csv");
+    std::ifstream database("/home/jorughen/Documents/progra3/datos.csv");
     if(!database.is_open()) {
         std::cerr << "Error opening file: " << std::strerror(errno) << std::endl;
         return 1;
@@ -32,46 +36,29 @@ int main() {
 
                 Trie->insert_movies_data(synopsis, new_movie);
                 Trie->insert_movies_data(title, new_movie);
-                Trie->insert_movies_data(tags, new_movie);
+                TrieTags->insert_movies_data(tags, new_movie);
 
         }
     }
 
 
-//    auto title_root = Utils::deserializeTrie("C:\\Users\\flauta\\progra3\\proyecto\\progra3\\trie.txt");
+
 
     // Antes del bucle, abre un archivo de texto en modo de escritura
-    std::ofstream outFile("C:\\Users\\flauta\\progra3\\proyecto\\progra3\\output.txt");
+    std::ofstream outFile("/home/jorughen/Documents/progra3/tags.txt");
     if (!outFile.is_open()) {
         std::cerr << "Error opening file for writing." << std::endl;
         return 1; // O manejar el error como prefieras
     }
 
 // Dentro del bucle, escribe en el archivo
-    for (auto movie : Trie->search_movies_by_key("Jesse")) {
-        outFile << *movie << std::endl;
+    for (auto movie : Trie->search_movies_by_key("Walter White")) {
+        outFile << *movie.first << std::endl;
     }
 
 // Después del bucle, cierra el archivo
     outFile.close();
 
-
-    // Antes del bucle, abre un archivo de texto en modo de escritura
-    std::ofstream outFile2("C:\\Users\\flauta\\progra3\\proyecto\\progra3\\tags.txt");
-    if (!outFile2.is_open()) {
-        std::cerr << "Error opening file for writing." << std::endl;
-        return 1; // O manejar el error como prefieras
-    }
-
-// Dentro del bucle, escribe en el archivo
-    for (auto movie : Trie->search_movies_by_key("Horror Action")) {
-        outFile2 << *movie << std::endl;
-    }
-
-    Utils::serializeTrie(Trie, "C:\\Users\\flauta\\progra3\\proyecto\\progra3\\trie.txt");
-
-// Después del bucle, cierra el archivo
-    outFile2.close();
 
 
 
