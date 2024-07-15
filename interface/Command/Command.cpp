532cef8bf18cd6b60267f6dde856c48f5130c517
BadiRodriguez <166084491+Badi-Rodriguez@users.noreply.github.com>
2024-07-14 21:44:39 -0500

    Substition base
    + Modification of the optiosns for the Command module, following the structure given by the command_structure.txt file, read for further details.

--- interface/Command/Command.cpp
+++ interface/Command/Command.cpp
@@ -1,77 +1,83 @@
 #include "Command.h"
 #include "Menu.h"
+#include <iostream>
 
-// Implementaciones de Option1
-void Option1::execute() {
+// Implementaciones de SearchOption
+void SearchOption::execute() {
     std::vector<std::unique_ptr<Command>> commands;
-    commands.push_back(std::make_unique<SubOption1>());
-    commands.push_back(std::make_unique<SubOption2>());
-    commands.push_back(std::make_unique<SubOption5>());
+    commands.push_back(std::make_unique<SearchByTagsSubOption>());
+    commands.push_back(std::make_unique<SearchByNameSubOption>());
+    commands.push_back(std::make_unique<SearchBySynopsisSubOption>());
     commands.push_back(std::make_unique<ReturnOption>());
     Menu::getInstance().setCommands(std::move(commands));
 }
 
-// Implementaciones de Option2
-void Option2::execute() {
+// Implementaciones de CheckBookmarksOption
+void CheckBookmarksOption::execute() {
     std::vector<std::unique_ptr<Command>> commands;
-    commands.push_back(std::make_unique<SubOption3>());
-    commands.push_back(std::make_unique<SubOption4>());
+    commands.push_back(std::make_unique<WatchLaterOption>());
+    commands.push_back(std::make_unique<FavoritesOption>());
+    commands.push_back(std::make_unique<ReturnOption>());
+    Menu::getInstance().setCommands(std::move(commands));
+}
+
+// Implementaciones de SeeMoreOption
+void SeeMoreOption::execute() {
+    std::vector<std::unique_ptr<Command>> commands;
+    commands.push_back(std::make_unique<DevModeOption>());
+    commands.push_back(std::make_unique<WhatIsChavezNetOption>());
     commands.push_back(std::make_unique<ReturnOption>());
     Menu::getInstance().setCommands(std::move(commands));
 }
 
 // Implementaciones de ExitOption
 void ExitOption::execute() {
-    endwin();
+    std::cout << "Exiting application..." << std::endl;
     exit(0);
 }
 
-// Implementaciones de SubOption1
-void SubOption1::execute() {
-    std::vector<std::unique_ptr<Command>> commands;
-    commands.push_back(std::make_unique<SubSubOption1>());
-    commands.push_back(std::make_unique<SubSubOption2>());
-    commands.push_back(std::make_unique<SubSubOption5>());
-    commands.push_back(std::make_unique<ReturnOption>());
-    Menu::getInstance().setCommands(std::move(commands));
+// Implementaciones de SearchByTagsSubOption
+void SearchByTagsSubOption::execute() {
+    std::cout << "Executing Search by Tags" << std::endl;
 }
 
-// Implementaciones de SubOption2
-void SubOption2::execute() {
-    // Define actions for SubOption2
+// Implementaciones de SearchByNameSubOption
+void SearchByNameSubOption::execute() {
+    std::cout << "Executing Search by Name" << std::endl;
 }
 
-// Implementaciones de SubOption3
-void SubOption3::execute() {
-    // Define actions for SubOption3
+// Implementaciones de SearchBySynopsisSubOption
+void SearchBySynopsisSubOption::execute() {
+    std::cout << "Executing Search by Synopsis" << std::endl;
 }
 
-// Implementaciones de SubOption4
-void SubOption4::execute() {
-    // Define actions for SubOption4
+// Implementaciones de ReturnSubOption
+void ReturnSubOption::execute() {
+    Menu::getInstance().restoreState();
 }
 
-// Implementaciones de SubOption5
-void SubOption5::execute() {
-    // Define actions for SubOption5
+// Implementaciones de WatchLaterOption
+void WatchLaterOption::execute() {
+    std::cout << "Executing Watch Later Option" << std::endl;
 }
 
-// Implementaciones de SubSubOption1
-void SubSubOption1::execute() {
-    // Define actions for SubSubOption1
+// Implementaciones de FavoritesOption
+void FavoritesOption::execute() {
+    std::cout << "Executing Favorites Option" << std::endl;
 }
 
-// Implementaciones de SubSubOption2
-void SubSubOption2::execute() {
-    // Define actions for SubSubOption2
+// Implementaciones de DevModeOption
+void DevModeOption::execute() {
+    std::cout << "Executing DevMode Option" << std::endl;
 }
 
-// Implementaciones de SubSubOption5
-void SubSubOption5::execute() {
-    // Define actions for SubSubOption5
+// Implementaciones de WhatIsChavezNetOption
+void WhatIsChavezNetOption::execute() {
+    std::cout << "Executing What is ChavezNet Option" << std::endl;
 }
 
 // Implementaciones de ReturnOption
 void ReturnOption::execute() {
     Menu::getInstance().restoreState();
 }
+
