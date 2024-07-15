f2d6629ad3fd1f6e91910557ddc1362eac2f4f38
BadiRodriguez <166084491+Badi-Rodriguez@users.noreply.github.com>
2024-07-14 21:15:21 -0500

    Guides for buttons
    No problem Paca >:3

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,16 @@
 #include "tools/Utils.h"
 #include <vector>
 #include <filesystem>
+#include "../interface/Menu/Menu.h"
 
 namespace fs = std::filesystem;
 
 
 
 int main(){
+
+
+
     fs::path projectDir = fs::absolute(fs::path(__FILE__).parent_path().parent_path());
 
     auto* TrieData = new TrieNode();
